322a8860567c4df2ae23615dc55c8489afae5df8
Borja Rodrigo <borja.rodrigo@voicemod.net>
2025-03-12 13:57:51 +0100

    Image routes with env variable. Max height for Who section.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   site: 'https://zordycor.github.io',
-  base: 'cuallado',
+  base: import.meta.env.MODE !== 'development' ? 'cuallado' : '/',
   outDir: './docs'
 });
